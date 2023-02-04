e72757b617737939339a4d0d862351065014af9a
Ayosef <syrivera@uc.cl>
2023-02-04 14:20:17 -0300

    Add the win condition to whoever have a score of 5

--- index.js
+++ index.js
@@ -4,46 +4,72 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random()*choice.length)];
 }
 
-// create variables for player and computer selection
+// create variables for player, computer selection and scores.
 let computerSelection;
 let playerSelection;
+let player = 0;
+let computer = 0;
 
 
 
 //create selector
 const btnChoice = document.querySelectorAll('.btn');
 const result = document.querySelector('.result');
+const score = document.querySelector('.score');
+const playerChoice = document.querySelector('.playerChoice');
+const computerChoice = document.querySelector('.computerChoice');
+const winner = document.querySelector('.winner');
+
 
 //add an event for each time u click a button
 btnChoice.forEach( button => button.addEventListener('click', () => {
-    playerSelection = button.value.toLowerCase();
-    console.log({playerSelection});
-    computerSelection = getComputerChoice();
-    console.log({computerSelection});
-    result.textContent = playRound(computerSelection,playerSelection);
+    if (player < 5 && computer < 5) {
+        playerSelection = button.value.toLowerCase();
+        computerSelection = getComputerChoice();
+        result.textContent = playRound(computerSelection,playerSelection);
+        score.textContent = `Score: Player ${player}-${computer} Computer`;
+        playerChoice.textContent = `Player: ${playerSelection}`;
+        computerChoice.textContent = `Computer: ${computerSelection}`;
+        if (player === 5) {
+            winner.textContent = 'Player Wins!';
+        }
+        else if (computer === 5) {
+            winner.textContent = 'Computer Wins!';
+        }
+    }
+    else {
+        return;
+    };
 }));
 
 //make the function to compare computer selection vs player selection
 
 function playRound (computerSelection,playerSelection) {   
     
+
     if (computerSelection === "rock" && playerSelection === "paper") {
-        return "You win! paper beats rock"
+        player+=1;
+        return "You win! paper beats rock";
     }
     else if (computerSelection === "rock" && playerSelection === "scissors") {
-        return "You lose! rock beats scissors"
+        computer+=1;
+        return "You lose! rock beats scissors";
     }
     else if (computerSelection === "paper" && playerSelection === "rock") {
-        return "You lose! paper beats rock"
+        computer+=1;
+        return "You lose! paper beats rock";
     }
     else if (computerSelection === "paper" && playerSelection === "scissors") {
-        return "You win! scissors beats paper"
+        player+=1;
+        return "You win! scissors beats paper";
     }
     else if (computerSelection === "scissors" && playerSelection === "rock") {
-        return "You win! rock beats scissors"
+        player+=1;
+        return "You win! rock beats scissors";
     }
     else if (computerSelection === "scissors" && playerSelection === "paper") {
-        return "You lose! scissors beats paper"
+        computer+=1;
+        return "You lose! scissors beats paper";
     }
     else if (computerSelection === playerSelection) {
         return "No one wins, it's a draw"
@@ -52,49 +78,3 @@ function playRound (computerSelection,playerSelection) {
         return "Error"
     }
 }
-
-//console.log(playRound(computerSelection,playerSelection));
-
-let player = 0;
-let computer = 0;
-let draw = 0;
-
-
-/*function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice(choice);
-        console.log(computerSelection);
-        let playerSelection = prompt("Choose between rock, paper, scissors");
-        playerSelection = playerSelection.toLowerCase();
-        console.log(playerSelection);
-        console.log(playRound(computerSelection,playerSelection));
-        
-
-        if (playRound(computerSelection,playerSelection).startsWith("You win!")) {
-            player +=1
-        }
-        else if (playRound(computerSelection,playerSelection).startsWith("You lose!")) {
-            computer +=1
-        }
-        else {
-            draw +=1
-        }
-        
-        
-        console.log(`Player : ${player}`);
-        console.log(`Computer : ${computer}`);
-        console.log(`Draw : ${draw}`);
-}
-
-    if (player > computer) {
-        console.log(`Player Wins: ${player}-${computer}`)
-    }
-    else if (player < computer) {
-        console.log(`Computer Wins: ${computer}-${player}`)
-    }
-    else {
-        console.log(`Draw: ${player}-${computer}`)
-    }
-}
-
-game()*/
